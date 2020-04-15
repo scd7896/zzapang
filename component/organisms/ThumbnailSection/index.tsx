4d5858d7e7e03b8b66cb918bcfa8711a915173ec
scd7896 <scd1212@naver.com>
2020-04-15 21:26:21 +0900

    이미지 레이지로딩 구현완료

--- component/organisms/ThumbnailSection/index.tsx
+++ component/organisms/ThumbnailSection/index.tsx
@@ -1,18 +1,31 @@
 import * as React from 'react'
+import { useState, useEffect } from 'react'
 import ThumbnailBigImage from '../../atoms/ThumbnailBigImage';
 import SnapshotThumbnailBox from '../../molecules/SnapshotThumbnailBox'
 // Thumnbnail 한줄 전체 섹션 썸네일 큰 이미지와 작은이미지를 받는다. 그리고 2초마다 큰이미지의 셀렉트를 바꾼다
-const ThumbnailSection = ({ bigThumbnailImageDatas, snapShotDatas }: ThumbnailSectionProps) => {
-
+const ThumbnailSection = ({ bigThumbnailImageDatas, snapShotDatas, nowSelect }: ThumbnailSectionProps) => {
+    const [cnt, setCnt] = useState(0);
+    useEffect(()=>{
+        if(cnt > bigThumbnailImageDatas.length) {
+            return;
+        }
+        
+        setCnt(cnt+1);
+    },[nowSelect])
     return (
         <section>
             {
-                bigThumbnailImageDatas.map((bigThumbnailImageData) => {
-                    return <ThumbnailBigImage {...bigThumbnailImageData} />
+                bigThumbnailImageDatas.map((bigThumbnailImageData, i) => {
+                    console.log('vv',cnt, nowSelect)
+                    return <ThumbnailBigImage 
+                        bigImageUrl={bigThumbnailImageData.bigImageUrl}
+                        isLoading={cnt > i}
+                        isSelected={i === nowSelect}
+                        key={i} />
                 })
             }
             <div>
-                <SnapshotThumbnailBox snapShotDatas={snapShotDatas} />
+                
             </div>
         </section>
     )
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,24 +1,50 @@
 import * as React from 'react'
+import { useEffect, useState } from 'react';
 import { NextPage } from 'next';
-import { useDispatch } from 'react-redux';
-
-import { Action } from 'redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { ThumbnailList } from '../dummyData'
-import { setTestTumbnail } from '../action';
+import { setTestTumbnail, rotateThumbnail } from '../action';
+import ThumbnailSection from '../component/organisms/ThumbnailSection'
 const Home: NextPage<{ userAgent: string }> = ({ userAgent }) => {
+    const thumbnail: Thumbnail = useSelector<RootStore>(state => state.thumbnail) as Thumbnail;    
+    const [timer, setTimer] = useState<NodeJS.Timeout>();
     const dispatch = useDispatch();
-    React.useEffect(()=>{
-        
+    useEffect(() => {
+        setTimer(setTimeout(()=>{
+            dispatch(rotateThumbnail())
+        }, 2500))
     },[])
     return(
         <div>
             hello world
             <p>userAgent : { userAgent }</p>
+            <ThumbnailSection 
+                nowSelect={thumbnail.nowSelect}
+                bigThumbnailImageDatas={thumbnail.bigImageDatas}
+                snapShotDatas={thumbnail.snapShotDatas}
+            />
         </div>
     )
 }
-Home.getInitialProps = async ({ req }) => {
+Home.getInitialProps = async ({req, store}) => {
     const userAgent = req ? req.headers['user-agent'] || "" : navigator.userAgent;
+    const bigImageDatas = ThumbnailList.bigThumbnailImageDatas.map((el) => {
+        return {
+            bigImageUrl: el.bigImageUrl
+        }
+    });
+    
+    const snapshotImageDatas = ThumbnailList.snapShotDatas.map((el) => {
+        return {
+            imageUrl: el.imageUrl,
+            title: el.title
+        }
+    })
+    const payload = {
+        bigImageDatas: bigImageDatas,
+        snapShotImageDatas: snapshotImageDatas
+    }
+    store.dispatch(setTestTumbnail(payload));
     
     return { userAgent }
 }
