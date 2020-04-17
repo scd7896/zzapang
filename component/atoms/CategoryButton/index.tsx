eda8a0a2a25c070a2de347fb0a49ee0f4c653d71
scd7896 <scd1212@naver.com>
2020-04-17 16:53:40 +0900

    카테고리 버튼 컴포넌트

---
+++ component/atoms/CategoryButton/index.tsx
@@ -0,0 +1,18 @@
+import * as React from 'react'
+import './styles.scss'
+type CategoryButtonProps = {
+    onMouseOver: Function
+}
+const CategoryButton = ({ onMouseOver }: CategoryButtonProps) => {
+    
+    const mouseOver = () => {
+        onMouseOver()
+    }
+
+    return (
+        <div className="category-button-wrapper" onMouseOver={mouseOver}>
+            <p className="category-text-contents">카테고리</p>
+        </div>
+    )
+}
+export default CategoryButton
