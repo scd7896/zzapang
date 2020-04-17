30435768b19cd23611a4f53a7f3deabd3cff6c0e
scd7896 <scd1212@naver.com>
2020-04-17 14:52:57 +0900

    헤더 작업

---
+++ component/template/HeaderLayout/index.tsx
@@ -0,0 +1,16 @@
+import * as React from 'react'
+import './styles.scss'
+const HeaderLayout = () => {
+    return (
+        <header>
+            <section className="header-content-section">
+                <h2>헬로 난 헤더야</h2>
+            </section>
+            <article className="top-bar">
+                
+            </article>
+        </header>
+    )
+}
+
+export default HeaderLayout
