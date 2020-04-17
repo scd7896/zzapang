b6ddd0550801d8c02a6b3f320bee8068087d5f1e
scd7896 <scd1212@naver.com>
2020-04-17 12:38:20 +0900

    더미데이터 작성

--- dummyData/index.ts
+++ dummyData/index.ts
@@ -1,5 +1,34 @@
 
-
+export const TodayList = {
+    todayListDatas: [{
+        imageUrl: 'https://static.coupangcdn.com/pa/cmg_paperboy/image/1586766587707/0416_C2_2_%EB%A6%AC%EC%8A%A4%ED%85%8C%EB%A6%B0.jpg',
+        href: 'https://www.coupang.com/np/promotion/51744?from=home_C2&traid=home_C2'
+    },{
+        imageUrl: 'https://static.coupangcdn.com/pa/cmg_paperboy/image/1586766587707/0416_C2_1_%EB%82%A8%EC%96%91%EC%9C%A0%EC%97%85.jpg',
+        href: 'https://pages.coupang.com/p/9594?from=home_C2&traid=home_C2'
+    },{
+        imageUrl: 'https://static.coupangcdn.com/pa/cmg_paperboy/image/1586766587707/0416_C2_3_%EC%A7%91%EC%9C%BC%EB%A1%9C%EC%98%A8.jpg',
+        href: 'https://pages.coupang.com/p/9273?from=home_C2&traid=home_C2'
+    },{
+        imageUrl: 'https://static.coupangcdn.com/pa/cmg_paperboy/image/1586766587707/0416_C2_4_%EC%A2%8B%EC%9D%80%EC%B9%AB%EC%86%94.jpg',
+        href: 'https://pages.coupang.com/p/9166?from=home_C2&traid=home_C2'
+    },{
+        imageUrl: 'https://static.coupangcdn.com/pa/cmg_paperboy/image/1586766587707/0416_C2_5_%EC%82%BC%EC%84%B1%EC%94%A8%EC%95%A4%EC%97%90%EC%9D%B4%EC%B9%98.jpg',
+        href: 'https://pages.coupang.com/p/9696?from=home_C2&traid=home_C2'
+    },{
+        imageUrl: 'https://static.coupangcdn.com/pa/cmg_paperboy/image/1586766587707/0416_C2_6_%EC%A3%BC%ED%8F%AC%EB%94%94%EC%95%84%EC%9D%B4.jpg',
+        href: 'https://pages.coupang.com/p/9469?from=home_C2&traid=home_C2'
+    },{
+        imageUrl: 'https://static.coupangcdn.com/pa/cmg_paperboy/image/1586766587707/0416_C2_7_%EC%83%98%ED%91%9C.jpg',
+        href: 'https://pages.coupang.com/p/9377?from=home_C2&traid=home_C2',
+    },{
+        imageUrl: 'https://static.coupangcdn.com/za/cmg_paperboy/image/1586847492073/0416_C2_8_%E1%84%82%E1%85%A2%E1%84%8E%E1%85%AE%E1%84%85%E1%85%A5%E1%86%AF%E1%84%87%E1%85%A1%E1%86%AF%E1%84%85%E1%85%A1%E1%86%AB%E1%84%89%E1%85%B3.jpg',
+        href: 'https://pages.coupang.com/p/8903?from=home_C2&traid=home_C2'
+    },{
+        imageUrl: 'https://static.coupangcdn.com/pa/cmg_paperboy/image/1586766587707/0416_C2_9_%EC%9D%B8%ED%84%B0%ED%8E%AB.jpg',
+        href: 'https://pages.coupang.com/p/8958?from=home_C2&traid=home_C2'
+    }]
+}
 export const ThumbnailList = {
     nowSelect: 0,
     bigThumbnailImageDatas: [{
