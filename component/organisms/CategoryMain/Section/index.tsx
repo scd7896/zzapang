05ef6db092d96559076c22feca2c3ed8a309a277
scd7896 <scd1212@naver.com>
2020-04-30 23:36:16 +0900

    스크롤로 나중에 카테고리 나중에 렌더 하도록 구현

--- component/organisms/CategoryMain/Section/index.tsx
+++ component/organisms/CategoryMain/Section/index.tsx
@@ -8,14 +8,15 @@ import CategoryMainBigImage from '../../../atoms/CategoryMain/BigImage';
 
 interface CategorySectionProps {
   categoryItems: CategoryMainItemProps[],
-  bigImageUrl: string
+  bigImageUrl: string,
+  color: string
 }
 
-const CategorySection = ({ categoryItems, bigImageUrl }: CategorySectionProps) => {
+const CategorySection = ({ categoryItems, bigImageUrl, color }: CategorySectionProps) => {
   return (
     <section className="category-section-wrapper"
-    style={{borderTop: '3px solid #878700'}}>
-      <CategoryMainTitle text="테스트카테고리" url="https://www.naver.com" color="#878700" />
+    style={{borderTop: '2px solid '+color}}>
+      <CategoryMainTitle text="테스트카테고리" url="https://www.naver.com" color={color} />
       <CategoryMainBigImage url={bigImageUrl} />
       <CategoryMainItems categoryItems={categoryItems} />
     </section>
--- component/template/root/index.tsx
+++ component/template/root/index.tsx
@@ -6,13 +6,46 @@ import TodayHeadImage from "../../atoms/TodayHeadImage";
 import CategoryNav from "../../molecules/CategoryNav";
 import CategorySection from "../../organisms/CategoryMain/Section";
 import { oneCategoryItem, bigImage } from '../../../dummyData/index';
-
+import { getRandomColor } from '../../../util/index';
 import "./styles.scss";
+
 type IndexTemplateProps = {
   thumbnail: Thumbnail;
 };
+
 const categoryDummys = [oneCategoryItem,oneCategoryItem,oneCategoryItem,oneCategoryItem,oneCategoryItem,oneCategoryItem];
 const Index = ({ thumbnail }: IndexTemplateProps) => {
+  const [randomColors, setRandomColors] = useState<Array<string>>([]);
+  const [isCategoryRender, setIsCategoryRender] = useState<boolean>(false);
+  const categoryScrollHandler = (event: Event) => {
+    if (isCategoryRender) {
+      
+      window.removeEventListener('scroll', categoryScrollHandler);
+      return;
+    }
+    const scrollHeight = document.documentElement.scrollHeight;
+    const clientHeight = document.documentElement.clientHeight;
+    const scrollTop = document.documentElement.scrollTop;
+    const scrollPos = scrollHeight - scrollTop;
+    if (clientHeight + 80 > scrollPos) {
+      setIsCategoryRender(true);
+      window.removeEventListener('scroll', categoryScrollHandler);
+    }
+  }
+
+  useEffect(()=>{
+    const nextArr = []
+    for (let i = 0 ; i < 9; i++) {
+      nextArr.push(getRandomColor())
+    }
+    setRandomColors(nextArr);
+
+    window.addEventListener('scroll', categoryScrollHandler)
+
+    return () => {
+      window.removeEventListener('scroll', categoryScrollHandler);
+    }
+  }, [])
   return (
     <section className="main-template-wrapper">
       <ThumbnailSection
@@ -25,10 +58,16 @@ const Index = ({ thumbnail }: IndexTemplateProps) => {
           <TodayHeadImage href="" isSelected={true} />
         </article>
       </section>
-      <section className="main-categoty-view">
-        <CategoryNav />
-        <CategorySection categoryItems={categoryDummys} bigImageUrl={bigImage}/>
-      </section>
+      { isCategoryRender && 
+        <section className="main-categoty-view">
+          <h1>카테고리 전체 보기</h1>
+          <CategoryNav />
+          { randomColors &&
+          randomColors.map((color, index) => {
+            return <CategorySection categoryItems={categoryDummys} bigImageUrl={bigImage} color={color} key={index}/>  
+          })}
+        </section>
+      }
     </section>
   );
 };
