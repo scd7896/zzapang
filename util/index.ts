c1b1cf3224e3cdf9dbd4819a3efdf4d557e72949
scd7896 <scd1212@naver.com>
2020-05-02 19:00:32 +0900

    검색 페이지 생성, 장바구니를 위한 쿠키 utill함수 작성

--- util/index.ts
+++ util/index.ts
@@ -7,3 +7,29 @@ export const getRandomColor = (): string => {
   }
   return color;
 }
+export const setCookie = (name: string, value: string, day: number) => {
+
+  // 변수를 선언한다.
+  const date = new Date();
+  date.setDate(date.getDate() + day);
+
+  let willCookie = "";
+  willCookie += name + "=" + encodeURIComponent(value) + ";";
+  willCookie += "Expires=" + date.toUTCString() + "";
+
+  // 쿠키에 넣습니다.
+  document.cookie = willCookie;
+}
+
+export const getCookie = (name: string) => {
+
+  // 변수를 선언한다.
+  const cookies = document.cookie.split(";");
+
+  // 쿠키를 추출한다.
+  for(let i in cookies) {
+      if(cookies[i].search(name) != -1) {
+        return decodeURIComponent(cookies[i].replace(name + "=", ""));
+      }
+  }
+}
