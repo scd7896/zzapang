1ba4c887aed9a14570cbb0218dd719f9d737794e
scd7896 <scd1212@naver.com>
2020-05-02 18:34:08 +0900

    카테고리 이미지 스크롤 위아래로 할 경우, 데이터 재요청하는 것 방지

--- component/atoms/CategoryMain/Image/index.tsx
+++ component/atoms/CategoryMain/Image/index.tsx
@@ -1,14 +1,16 @@
 import * as React from 'react';
-import { useRef, useEffect } from 'react';
+import { useRef, useEffect, useState } from 'react';
 import './styles.scss';
 
 const CategoryMainImage = ({ url, isRender }: ImageComponentProps) => {
   const ref = useRef<HTMLImageElement | null>(null)
+  const [isFirst, setIsFirst] = useState<boolean>(true)
   useEffect(() => {
-    if (isRender) {
+    if (isRender && isFirst) {
       ref.current!.src = ref.current!.dataset.src!;
+      setIsFirst(false);
     }
-  }, [isRender])
+  }, [isRender, isFirst])
   return (
     <div className="category-nomal-image">
       <img ref={ref} data-src={url}/>
